2d77bc8a3e4e2eb07e9d223ffef61c577e31de0b
feargrinn <tobyrogers000@gmail.com>
2020-12-09 10:59:41 +0100

    zaj 09.12

--- zaj_2020_12_09.py
+++ zaj_2020_12_09.py
@@ -42,3 +42,43 @@ for count, substring in enumerate(result_3, 1):
 
 #########################################################
 
+# print(bridge_of_death)
+
+pattern_1 = re.compile('-Jaki', re.IGNORECASE)
+result_0 = pattern_1.match(bridge_of_death)
+print(result_0)
+# match returned: None
+
+pattern_1 = re.compile('\n-Jaki', re.IGNORECASE)
+result_0 = pattern_1.match(bridge_of_death)
+print(result_0)
+
+pattern_1 = re.compile('Niebieski.', re.IGNORECASE)
+result_0 = pattern_1.finditer(bridge_of_death)
+for substring in result_0:
+    print(substring)
+
+pattern_1 = re.compile('Niebieski.$', re.MULTILINE)
+result_0 = pattern_1.finditer(bridge_of_death)
+for substring in result_0:
+    print(substring)
+
+#########################################################
+
+var_name = input('podaj nazwe: ')
+pattern_var = re.compile(r'^[a-z_][a-z_A-Z0-9]*$')
+result_var = pattern_var.search(var_name)
+if result_var:
+    result_word = result_var.group()
+    if re.match(r'_+$', result_word):
+        print('niby moze ale nie probuj')
+    else:
+        print('input moze byc nazwa zmiennej'.format(result_word))
+else:
+    print('input nie moze byc nazwa zmiennej'.format(var_name))
+
+#########################################################
+
+# zadanie
+# znajdz wszystkie wyrazy ktore skladaja sie z przynajmniej 4 znakow
+pattern_four = re.compile(r'/b/w/w/w/w+/b')
